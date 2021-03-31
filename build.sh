f8a1a6e4c319f8712b4de36487cb94ba8137e47f
anurnomeru <anurnomeru@163.com>
2021-03-31 10:43:04 +0800

    feat: supports dockerfile with zsh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+docker build -f ./golang/Dockerfile . -t golang --no-cache
+docker build -f ./java/Dockerfile . -t java --no-cache
+docker build -f ./node/Dockerfile . -t node --no-cache
+docker build -f ./python/Dockerfile . -t python --no-cache
+docker build -f ./ruby/Dockerfile . -t ruby --no-cache
+
