8ca25f3bbbb8b277d026659fc632a00c06244ed3
windghoul <windghoul@gmail.com>
2022-05-13 17:27:02 +0800

    FIX: Resolve duplicate installation dependencies

--- zsh/zsh-in-docker.sh
+++ zsh/zsh-in-docker.sh
@@ -29,60 +29,6 @@ echo "  THEME   = $THEME"
 echo "  PLUGINS = $PLUGINS"
 echo
 
-check_dist() {
-    (
-        . /etc/os-release
-        echo $ID
-    )
-}
-
-check_version() {
-    (
-        . /etc/os-release
-        echo $VERSION_ID
-    )
-}
-
-install_dependencies() {
-    DIST=`check_dist`
-    VERSION=`check_version`
-    echo "###### Installing dependencies for $DIST"
-
-    if [ "`id -u`" = "0" ]; then
-        Sudo=''
-    elif which sudo; then
-        Sudo='sudo'
-    else
-        echo "WARNING: 'sudo' command not found. Skipping the installation of dependencies. "
-        echo "If this fails, you need to do one of these options:"
-        echo "   1) Install 'sudo' before calling this script"
-        echo "OR"
-        echo "   2) Install the required dependencies: git curl zsh"
-        return
-    fi
-
-    case $DIST in
-        alpine)
-            $Sudo apk add --update --no-cache git curl zsh
-        ;;
-        centos | amzn)
-            $Sudo yum update -y
-            $Sudo yum install -y git curl
-            $Sudo yum install -y ncurses-compat-libs # this is required for AMZN Linux (ref: https://github.com/emqx/emqx/issues/2503)
-            $Sudo curl http://mirror.ghettoforge.org/distributions/gf/el/7/plus/x86_64/zsh-5.1-1.gf.el7.x86_64.rpm > zsh-5.1-1.gf.el7.x86_64.rpm
-            $Sudo rpm -i zsh-5.1-1.gf.el7.x86_64.rpm
-            $Sudo rm zsh-5.1-1.gf.el7.x86_64.rpm
-        ;;
-        *)
-            $Sudo apt-get update
-            $Sudo apt-get -y --force-yes install git curl zsh locales
-            if [ "$VERSION" != "14.04" ]; then
-                $Sudo apt-get -y  --force-yes install locales-all
-            fi
-            $Sudo locale-gen en_US.UTF-8
-    esac
-}
-
 zshrc_template() {
     _HOME=$1;
     _THEME=$2; shift; shift
@@ -123,7 +69,6 @@ powerline10k_config_end() {
 EOM
 }
 
-install_dependencies
 
 cd /tmp
 
